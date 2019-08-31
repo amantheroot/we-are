38a533a0a3adc3d94e81398196164cda4425b4c8
amantheroot <amantheroot@gmail.com>
2019-08-31 21:10:42 +0530

    Inital Commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://root:root@cluster0-rfohz.mongodb.net/test?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
